557494d7ff1a8c4c3e20813759637729fbe1f53f
Julien Panis <julienpanis@hotmail.com>
2021-02-09 20:14:48 +0100

    Bug fix : TIM_Handle configured BEFORE being passed as a function parameter !
    Signed-off-by: Julien Panis <julienpanis@hotmail.com>

--- target/Src/pl_time.c
+++ target/Src/pl_time.c
@@ -9,25 +9,13 @@
 // -------------------- Global variables --------------------
 static uint32_t guiCurrentTime;
 
-static TIM_HandleTypeDef grTIM_Handle = {0};
+static TIM_HandleTypeDef grTIM_Handle;
 
 
 // -------------------- HAL Functions --------------------
 void pl_time_init(void)
 {
 	guiCurrentTime = 0;
-	
-	// Enable TIM1 clock
-	__HAL_RCC_TIM1_CLK_ENABLE();
-	
-	// Enable TIM1 peripheral
-	__HAL_TIM_ENABLE(&grTIM_Handle);
-	
-	// Enable TIM1 Update IRQ
-	__HAL_TIM_ENABLE_IT(&grTIM_Handle, TIM_IT_UPDATE);
-	
-	// Enable TIM1 Update IRQ in NVIC
-	HAL_NVIC_EnableIRQ(TIM1_UP_IRQn);
 	
 	// Configure TIM1
 	grTIM_Handle.Instance = TIM1;
@@ -39,6 +27,19 @@ void pl_time_init(void)
 	grTIM_Handle.Init.RepetitionCounter = 0;
 	grTIM_Handle.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
 	
+	// Enable TIM1 clock
+	__TIM1_CLK_ENABLE();
+	
+	// Enable TIM1 peripheral
+	__HAL_TIM_ENABLE(&grTIM_Handle);
+	
+	// Enable TIM1 Update IRQ
+	__HAL_TIM_ENABLE_IT(&grTIM_Handle, TIM_IT_UPDATE);
+	
+	// Enable TIM1 Update IRQ in NVIC
+	HAL_NVIC_EnableIRQ(TIM1_UP_IRQn);
+	
+	// Initialize TIM1
 	HAL_TIM_Base_Init(&grTIM_Handle);
 	
 	// Start TIM1
@@ -63,7 +64,7 @@ void pl_time_waitMs(uint32_t uiDuration)
 void pl_time_deinit(void)
 {
 	HAL_TIM_Base_Stop_IT(&grTIM_Handle);
-	
+
 	HAL_TIM_Base_DeInit(&grTIM_Handle);
 	
 	HAL_NVIC_DisableIRQ(TIM1_UP_IRQn);
@@ -72,14 +73,11 @@ void pl_time_deinit(void)
 	
 	__HAL_TIM_DISABLE(&grTIM_Handle);
 	
-	__HAL_RCC_TIM1_CLK_DISABLE();
-	
-	guiCurrentTime = 0;
+	__TIM1_CLK_DISABLE();
 }
 
 
 // -------------------- Internal Functions --------------------
-
 void TIM1_UP_IRQHandler(void)
 {
 	guiCurrentTime += INC_MS;
