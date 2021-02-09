105019107a567acb36377598c09cc14508a6ac9c
Julien Panis <julienpanis@hotmail.com>
2021-02-09 20:11:42 +0100

    Minor modification
    Signed-off-by: Julien Panis <julienpanis@hotmail.com>

--- target/Src/pl_led.c
+++ target/Src/pl_led.c
@@ -13,13 +13,14 @@ void pl_led_init(void)
 	GPIO_InitTypeDef rGPIO_Init = {0};
 	
 	// Enable GPIOA clock
-	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__GPIOA_CLK_ENABLE();
 
 	// Configure LED GPIO
 	rGPIO_Init.Pin = LED_PIN;
 	rGPIO_Init.Mode = GPIO_MODE_OUTPUT_PP;
 	rGPIO_Init.Speed = GPIO_SPEED_FREQ_HIGH;
-	rGPIO_Init.Pull = GPIO_PULLUP;
+	
+	// Initialize LED GPIO
 	HAL_GPIO_Init(LED_PORT, &rGPIO_Init);	
 }
 
@@ -33,7 +34,5 @@ void pl_led_toggle(void)
 void pl_led_deinit(void)
 {
 	HAL_GPIO_DeInit(LED_PORT, LED_PIN);
-	
-	__HAL_RCC_GPIOA_CLK_DISABLE();
 }
 
